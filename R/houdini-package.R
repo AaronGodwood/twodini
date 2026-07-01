93aabac4d5258510b165b47abba0d65db9c04077
Aaron Godwood <godwoodaaron@gmail.com>
2026-07-01 11:42:10 +0100

    fixed dupliate definitions and c-path issues

--- R/houdini-package.R
+++ R/houdini-package.R
@@ -1,5 +1,9 @@
+#' @useDynLib houdini, .registration = TRUE
 #' @importFrom stats setNames
 #' @importFrom utils unzip
 #' @importFrom rstudioapi selectDirectory
 #' @importFrom xml2 read_xml write_xml xml_find_all xml_find_first xml_attr xml_name xml_parent xml_add_child xml_add_sibling xml_root
 "_PACKAGE"
+
+# Null-coalescing helper shared across the package.
+`%||%` <- function(a, b) if (!is.null(a)) a else b
